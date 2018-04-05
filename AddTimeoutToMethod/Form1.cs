f4b12da88efcea23306ab9fbd3c1c7320b7fc731
aucan <aucan@AUCAN>
2018-04-05 16:26:14 +0300

    ok

--- AddTimeoutToMethod/Form1.cs
+++ AddTimeoutToMethod/Form1.cs
@@ -5,6 +5,7 @@ using System.Data;
 using System.Drawing;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
@@ -15,6 +16,31 @@ namespace AddTimeoutToMethod
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string result = MethodWithTimeout("Test", 3);
+            MessageBox.Show(result);
+        }
+
+        private string MethodWithTimeout(string input, int timeout)
+        {
+            var task = Task.Run(() => SomeMethod(input));
+            if (task.Wait(TimeSpan.FromSeconds(timeout)))
+                return task.Result;
+            else
+                return "Time out!";
+        }
+
+        private string SomeMethod(string input)
+        {
+            string result = input ;
+            Random rnd= new Random();
+            int sleep = rnd.Next(100,4000);
+            MessageBox.Show("Result will send in :"+ sleep.ToString()+" ms");
+            Thread.Sleep(sleep);
+            return "Received : "+result;
         }
     }
 }
